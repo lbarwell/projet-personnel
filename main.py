dc6d03b325902d462130b49bf31152e485ebec65
Ludovic Barwell <ludovic.barwell@gmail.com>
2021-01-31 19:06:18 -0500

    Added player movement
    Players can move smoothly around the screen. They are not yet affected by gravity or obstacles.

--- main.py
+++ main.py
@@ -1,41 +1,33 @@
 import pygame, sys
 
+
+class Player():
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.surf = pygame.Surface((30, 70))
+        self.surf.fill((139, 0, 0))
+        self.rect = self.surf.get_rect()
+
+    def update(self, pressed_keys):
+        if pressed_keys[pygame.K_UP]:
+            self.y -= 5
+        if pressed_keys[pygame.K_DOWN]:
+            self.y += 5
+        if pressed_keys[pygame.K_LEFT]:
+            self.x -= 5
+        if pressed_keys[pygame.K_RIGHT]:
+            self.x += 5
+
+
 pygame.init()
+
 screen = pygame.display.set_mode((1200, 700))
 pygame.display.set_caption("Projet Personnel") # Name of game
 clock = pygame.time.Clock()
 
-# Import background
-background = pygame.image.load("Assets/basic-background.png").convert()
-screen.blit(background, (0, 0))
-
-# Import and display platforms
-ctr_left_platform = pygame.image.load("Assets/basic-mid-platform.png").convert()
-ctr_left_platform_rect = ctr_left_platform.get_rect(topleft = (150, 370))
-screen.blit(ctr_left_platform, ctr_left_platform_rect)
-
-ctr_right_platform = pygame.image.load("Assets/basic-mid-platform.png").convert()
-ctr_right_platform_rect = ctr_right_platform.get_rect(topleft = (675, 370))
-screen.blit(ctr_right_platform, ctr_right_platform_rect)
-
-bottom_platform = pygame.image.load("Assets/basic-low-platform.png").convert()
-bottom_platform_rect = bottom_platform.get_rect(topleft = (400, 545))
-screen.blit(bottom_platform, bottom_platform_rect)
-
-top_left_platform = pygame.image.load("Assets/basic-high-platform.png").convert()
-top_left_platform_rect = top_left_platform.get_rect(topleft = (350, 195))
-screen.blit(top_left_platform, top_left_platform_rect)
-
-top_right_platform = pygame.image.load("Assets/basic-high-platform.png").convert()
-top_right_platform_rect = top_right_platform.get_rect(topleft = (650, 195))
-screen.blit(top_right_platform, top_right_platform_rect)
-
-# Import players
-player1 = pygame.image.load("Assets/basic-red-player.png").convert()
-player1_rect = player1.get_rect(center = (200, 335))
-
-player2 = pygame.image.load("Assets/basic-blue-player.png").convert()
-player2_rect = player2.get_rect(center = (1000, 335))
+# Players
+player1 = Player(200, 300)
 
 while True:
     for event in pygame.event.get():
@@ -43,9 +35,35 @@ while True:
             pygame.quit()
             sys.exit()
 
+    # Background color
+    screen.fill((255, 255, 255))
+
+    # Platforms
+    cl_pf = pygame.Surface((375, 20))
+    cl_pf_rect = cl_pf.get_rect(topleft = (150, 370))
+    screen.blit(cl_pf, cl_pf_rect)
+
+    cr_pf = pygame.Surface((375, 20))
+    cr_pf_rect = cr_pf.get_rect(topleft = (675, 370))
+    screen.blit(cr_pf, cr_pf_rect)
+
+    b_pf = pygame.Surface((400, 20))
+    b_pf_rect = b_pf.get_rect(topleft = (400, 545))
+    screen.blit(b_pf, b_pf_rect)
+
+    tl_pf = pygame.Surface((200, 20))
+    tl_pf_rect = tl_pf.get_rect(topleft = (350, 195))
+    screen.blit(tl_pf, tl_pf_rect)
+
+    tr_pf = pygame.Surface((200, 20))
+    tl_pf_rect = tr_pf.get_rect(topleft = (650, 195))
+    screen.blit(tr_pf, tl_pf_rect)
+
     # Display players
-    screen.blit(player1, player1_rect)
-    screen.blit(player2, player2_rect)
+    pressed_keys = pygame.key.get_pressed()
+    player1.update(pressed_keys)
+
+    screen.blit(player1.surf, (player1.x, player1.y))
 
     pygame.display.update()
     clock.tick(60)
