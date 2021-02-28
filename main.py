0cc5218032f318665f683caf0b67e23911f5b36a
Ludovic Barwell <ludovic.barwell@gmail.com>
2021-02-28 11:31:07 -0500

    Fixed collisions and movement
    The player now lands on the platform (rather than in it).
    
    Added a settings file for better organization.
    
    Reversed the colours of the platforms and background because it looks nicer.

--- main.py
+++ main.py
@@ -1,57 +1,74 @@
 import pygame, sys
-
-wasd_ctrls = {
-    "move_left": pygame.K_LEFT,
-    "move_right": pygame.K_RIGHT,
-    "jump": pygame.K_UP,
-    "move_down": pygame.K_DOWN
-}
-
-arrow_ctrls = {
-    "move_left": pygame.K_a,
-    "move_right": pygame.K_d,
-    "jump": pygame.K_w,
-    "move_down": pygame.K_s
-}
+import settings
 
 
 class Game():
     def __init__(self):
-        self.topleft_platform_rect = top_platform.get_rect(topleft = (350, 190))
-        self.topright_platform_rect = top_platform.get_rect(topleft = (650, 190))
-        self.midleft_platform_rect = mid_platform.get_rect(topleft = (150, 370))
-        self.midright_platform_rect = mid_platform.get_rect(topleft = (675, 370))
-        self.bottom_platform_rect = bottom_platform.get_rect(topleft = (400, 550))
-        self.platforms = [self.topleft_platform_rect, self.topright_platform_rect, self.midleft_platform_rect, self.midright_platform_rect, self.bottom_platform_rect]
+        self.platforms = [
+            top_platform.get_rect(topleft = (350, 190)),
+            top_platform.get_rect(topleft = (650, 190)),
+            mid_platform.get_rect(topleft = (150, 370)),
+            mid_platform.get_rect(topleft = (675, 370)),
+            bottom_platform.get_rect(topleft = (400, 550))
+        ]
+
+
+    def forces(self, player):
+        # Gravity
+        player.y_vel += gravity
+
+        # Friction
+        if player.x_vel >= friction:
+            player.x_vel -= friction
+        elif player.x_vel <= -friction:
+            player.x_vel += friction
+        else:
+            player.x_vel = 0
+
+        # Max speed
+        if player.x_vel > max_xvel:
+            player.x_vel = max_xvel
+        elif player.x_vel < -max_xvel:
+            player.x_vel = -max_xvel
+        elif player.y_vel > max_yvel:
+            player.y_vel = max_yvel
+        elif player.y_vel < -max_yvel:
+            player.y_vel = -max_yvel
+
+
+    def collisions(self, player):
+        # Ground
+        if player.rect.bottom >= SCREEN_HEIGHT:
+            player.y_vel = 0
+            player.rect.y = SCREEN_HEIGHT - 100
+            player.collision = True
+        else:
+            player.collision = False
+
+        # Platform
+        for platform in self.platforms:
+            if player.rect.colliderect(platform) and player.y_vel >= 0 and player.rect.y < platform.top - 80:
+                player.y_vel = 0
+                player.rect.y = platform.top - 100
+                player.collision = True
+                break
 
 
 class Player():
     def __init__(self, x, y, player_image, ctrls):
-        self.x = x
-        self.y = y
         self.x_vel = 0
         self.y_vel = 0
         self.ctrls = ctrls
         self.surf = player_image
         self.rect = self.surf.get_rect(topleft = (x, y))
 
-    def update(self, pressed_keys):
-        # Collisions
-        if self.rect.bottom >= screen_height:
-            self.y_vel = 0
-            self.y = screen_height - 100
-            self.collision = True
-        else:
-            self.collision = False
 
-        for platform in game.platforms:
-            if self.rect.bottom >= platform.top and self.rect.bottom <= platform.bottom and self.rect.midbottom >= platform.midleft and self.rect.midbottom <= platform.midright:
-                self.y_vel = 0
-                self.y = platform.top - 100
-                self.collision = True
-                break
+    def update(self):
+        self.rect.centerx += self.x_vel
+        self.rect.centery += self.y_vel
 
-        # Movement
+
+    def move(self, pressed_keys):
         if pressed_keys[self.ctrls["move_left"]]:
             self.x_vel -= 0.5
         if pressed_keys[self.ctrls["move_right"]]:
@@ -61,55 +78,52 @@ class Player():
         if pressed_keys[self.ctrls["move_down"]]:
             self.y_vel += 0.5
 
-        self.y_vel += gravity
-
-        if self.x_vel >= friction:
-            self.x_vel -= friction
-        elif self.x_vel <= -friction:
-            self.x_vel += friction
-        else:
-            self.x_vel = 0
-
-        if self.x_vel > max_vel:
-            self.x_vel = max_vel
-        elif self.x_vel < -max_vel:
-            self.x_vel = -max_vel
-        elif self.y_vel > max_vel:
-            self.y_vel = max_vel
-        elif self.y_vel < -max_vel:
-            self.y_vel = -max_vel
-
-        self.rect.centerx += self.x_vel
-        self.rect.centery += self.y_vel
 
 # Game variables
-screen_width = 1200
-screen_height = 700
+SCREEN_WIDTH = 1200
+SCREEN_HEIGHT = 700
+PLAYER_WIDTH = 40
+PLAYER_HEIGHT = 100
 gravity = 0.4
 friction = 0.3
-max_vel = 14
+max_xvel = 10
+max_yvel = 14
 
 pygame.init()
 
-screen = pygame.display.set_mode((screen_width, screen_height))
-# Name of game
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Projet Personnel")
 clock = pygame.time.Clock()
 
 # Load images
 red_player = pygame.image.load("Assets/basic_red_player.png").convert_alpha()
+red_player = pygame.transform.scale(red_player, (PLAYER_WIDTH, PLAYER_HEIGHT))
+
 blue_player = pygame.image.load("Assets/basic_blue_player.png").convert_alpha()
+blue_player = pygame.transform.scale(blue_player, (PLAYER_WIDTH, PLAYER_HEIGHT))
 
 top_platform = pygame.image.load("Assets/basic_top_platform.png").convert_alpha()
 mid_platform = pygame.image.load("Assets/basic_mid_platform.png").convert_alpha()
 bottom_platform = pygame.image.load("Assets/basic_bottom_platform.png").convert_alpha()
 
+contact_0 = pygame.image.load("Assets/contact_0.png").convert_alpha()
+contact_0 = pygame.transform.scale(contact_0, (100, 100))
+
+contact_1 = pygame.image.load("Assets/contact_1.png").convert_alpha()
+contact_1 = pygame.transform.scale(contact_1, (100, 100))
+
+contact_2 = pygame.image.load("Assets/contact_2.png").convert_alpha()
+contact_2 = pygame.transform.scale(contact_2, (100, 100))
+
+contact_3 = pygame.image.load("Assets/contact_3.png").convert_alpha()
+contact_3 = pygame.transform.scale(contact_3, (100, 100))
+
 # Create game
 game = Game()
 
 # Create Players
-player1 = Player(1100, 100, blue_player, arrow_ctrls)
-player2 = Player(100, 100, red_player, wasd_ctrls)
+player1 = Player(1100, 100, blue_player, settings.controls["wasd"])
+player2 = Player(100, 100, red_player, settings.controls["arrows"])
 players = [player1, player2]
 
 while True:
@@ -119,20 +133,24 @@ while True:
             sys.exit()
 
     # Background
-    screen.fill((255, 255, 255))
+    screen.fill((0, 0, 0))
 
     # Platforms
-    screen.blit(top_platform, game.topleft_platform_rect)
-    screen.blit(top_platform, game.topright_platform_rect)
-    screen.blit(mid_platform, game.midleft_platform_rect)
-    screen.blit(mid_platform, game.midright_platform_rect)
-    screen.blit(bottom_platform, game.bottom_platform_rect)
+    screen.blit(top_platform, game.platforms[0])
+    screen.blit(top_platform, game.platforms[1])
+    screen.blit(mid_platform, game.platforms[2])
+    screen.blit(mid_platform, game.platforms[3])
+    screen.blit(bottom_platform, game.platforms[4])
 
     # Players
     for player in players:
-        screen.blit(player.surf, player.rect)
+        player.move(pygame.key.get_pressed())
+        game.forces(player)
+        player.update()
 
-        player.update(pygame.key.get_pressed())
+        game.collisions(player)
+
+        screen.blit(player.surf, player.rect)
 
     pygame.display.update()
     clock.tick(60)
---
+++ settings.py
@@ -0,0 +1,17 @@
+import pygame
+
+controls = {
+    "wasd": {
+        "move_left": pygame.K_LEFT,
+        "move_right": pygame.K_RIGHT,
+        "jump": pygame.K_UP,
+        "move_down": pygame.K_DOWN
+    },
+
+    "arrows": {
+        "move_left": pygame.K_a,
+        "move_right": pygame.K_d,
+        "jump": pygame.K_w,
+        "move_down": pygame.K_s
+    }
+}
